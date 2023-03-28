747a03ee766eb2665abdafd202766e7734774fc3
Ricardo Andrade <ricardo.andrade05@gmail.com>
2023-03-27 23:09:43 -0300

    Criando material table para listar Cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,17 @@
+import { Course } from './../model/course';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
-export class CoursesComponent {
 
+export class CoursesComponent {
+  courses: Course[] = [];
+  displayedColumns = [ 'name','category']
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
